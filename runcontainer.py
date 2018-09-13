8f0a28ed545f336dbd84b68a787d9c61503663f7
Alessandra Forti <aforti@vm26.in.tier2.hep.manchester.ac.uk>
2018-09-13 19:50:11 +0100

    Added input file part and some protection against weird commands with ;

--- runcontainer.py
+++ runcontainer.py
@@ -10,6 +10,7 @@
 # - Alessandra Forti, alessandra.forti@cern.ch, 2018
 
 import os
+import sys
 import time
 import argparse
 import logging
@@ -25,29 +26,45 @@ print time.ctime()
 #      --outputs out.dat --site=ANALY_MANC_SL7 --noBuild \
 #      --inDS user.aforti.test5_out.dat.197875784
 
+# Example of runGen command to process
+# -j "" --sourceURL https://aipanda078.cern.ch:25443 -r . \
+# -p "chmod%20777%20run_grid.sh%3B%20./run_grid.sh%20229%20input.in%20\
+# --proc%20wm%20--mbins%2032%2C40%2C200%20--pdfset%20ATLAS-epWZ16-EIG%20\
+# --pdfvar%200%20--order%202%20--kmufac%201%20--kmuren%201%20\
+# --verbose%20%3B" \
+# -l panda.0911074511.596244.lib._15353637.14628477940.lib.tgz \
+# -o "{'results.txt': 'user.sawebb.15353637._000229.results.txt', \
+# 'results.root': 'user.sawebb.15353637._000229.results.root'}"
+# --rootVer 6.04.18
+
+
 def main():
 
     """ Main function of run_container """
     sc = singularity_command()
-    run_container(sc) 
+    run_container(sc)
     rename_ouput()
 
 
 def singularity_command():
-    
+
     # Options for the command line string have default values or are mandatory
 
     # Base singularity command
-    singularity_base='singularity exec -C'
+    singularity_base = 'singularity -s exec -C'
 
     # If Cvmfs add that to bind_paths
     cvmfs = ''
-
     if args.ctr_cvmfs:
         cvmfs = '-B /cvmfs:/cvmfs'
-        
-    # Add Command it need to remove the 
-    command = urllib.unquote(args.ctr_cmd)
+
+    logging.debug("Command to run in the container %s", args.ctr_cmd)
+    if ';' in args.ctr_cmd:
+        logging.error("Multiple commands not allowed with " +
+                      "containers please use && to concatenate them")
+        sys.exit(1)
+
+    command = args.ctr_cmd.replace('%IN', input())
 
     singularity_cmd = "%s --pwd %s -B $PWD:%s %s %s %s" % \
                       (singularity_base,
@@ -57,49 +74,64 @@ def singularity_command():
                        args.ctr_image,
                        command)
 
-    print singularity_cmd
+    logging.debug("Singularity command: %s", singularity_cmd)
     return singularity_cmd
+
 
 def run_container(cmd=''):
 
     # Just in case singularity is not the only container we run
-    # os.system going to be deprecated (?) but subprocess requires more 
+    # os.system going to be deprecated (?) but subprocess requires more
     # thinking to handle $PWD.
     os.system(cmd)
 
 
 def input():
 
-    # place holder function. ATM if the file is there and the application knows 
-    # about it this function isn't needed. However even without directIn and 
-    # without PoolFileCatalogs. I expect the task input to be split by the system 
-    # and the application not knowing.
+    # place holder function. ATM if the file is there and the application knows
+    # about it this function isn't needed. However even without directIn and
+    # without PoolFileCatalogs. I expect the task input to be split
+    # by the system and the application not knowing.
     logging.info("Input files %s" % args.input_files)
+
+    input_string = ''
+    for file in args.input_files:
+
+        if os.path.isfile(file):
+            file = args.ctr_datadir+'/'+file
+            input_string += "%s," % file
+        else:
+            logging.info("Input file %s is missing", file)
+
+    input_string = input_string[:-1]
+
+    return input_string
 
 
 def rename_ouput():
 
     # Rename the output files. No need to move them to currentDir
-    # because we are already there. PFC and jobreport.json at a 
-    # later stage all jobs I checked have them empty anyway
-    for old_name,new_name in args.output_files.iteritems():
+    # because we are already there. PFC and jobreport.json at
+    # a later stage all jobs I checked have them empty anyway
+    for old_name, new_name in args.output_files.iteritems():
         # archive *
         if old_name.find('*') != -1:
             subprocess.check_output(['tar', 'cvfz', new_name, old_name])
         else:
             subprocess.check_output(['mv', new_name, old_name])
-    
+
 
 if __name__ == "__main__":
 
     arg_parser = argparse.ArgumentParser()
 
-    # Required arguments 
+    # Required arguments
     required = arg_parser.add_argument_group('required arguments')
 
     # Command to execute
     required.add_argument('-p',
                           dest='ctr_cmd',
+                          type=urllib.unquote,
                           required=True,
                           help='Command to execute in the container')
 
@@ -122,7 +154,7 @@ if __name__ == "__main__":
     arg_parser.add_argument('-i',
                             dest='input_files',
                             type=ast.literal_eval,
-                            default="",
+                            default="[]",
                             help='Input files')
 
     # Container data directory
@@ -171,11 +203,15 @@ if __name__ == "__main__":
     args, unknown = arg_parser.parse_known_args()
 
     # Setup the logging level
-    format_str='%(asctime)s | %(levelname)-8s | %(message)s'
+    format_str = '%(asctime)s | %(levelname)-8s | %(message)s'
     if args.debug:
-        logging.basicConfig(filename='run_container.log', level=logging.DEBUG, format=format_str)
+        logging.basicConfig(filename='run_container.log',
+                            level=logging.DEBUG,
+                            format=format_str)
     else:
-        logging.basicConfig(filename='run_container.log', level=logging.INFO, format=format_str)
+        logging.basicConfig(filename='run_container.log',
+                            level=logging.INFO,
+                            format=format_str)
 
     logging.info("Following arguments are unknown or unsupported %s" % unknown)
 
